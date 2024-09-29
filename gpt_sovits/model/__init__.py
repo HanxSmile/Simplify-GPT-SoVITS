d1c2f90549c0958d1c72d8164d7fe033e3d8209f
hanx_smile <hanx_smile@icloud.com>
2024-09-29 11:15:45 +0800

    debug

--- gpt_sovits/model/__init__.py
+++ gpt_sovits/model/__init__.py
@@ -1,7 +1,12 @@
 from .gpt_sovits import GPT_SoVITS
 from .fish_speech import FishSpeech
+from .llama import NaiveTransformer, DualARTransformer
+from .firefly import FireflyArchitecture
 
 __all__ = [
     'GPT_SoVITS',
     'FishSpeech',
+    'NaiveTransformer',
+    'DualARTransformer',
+    'FireflyArchitecture',
 ]
